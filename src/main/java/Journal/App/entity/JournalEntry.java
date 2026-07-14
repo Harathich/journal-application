f42d103c669fddc0b5a206d7564757b94312a9ce
Harathi Chavala <chavalaharathi05@gmail.com>
2026-07-14 11:51:15 +0530

    Lombok Annotation Processor

--- src/main/java/Journal/App/entity/JournalEntry.java
+++ src/main/java/Journal/App/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package Journal.App.entity;
 
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.cglib.core.Local;
 import org.springframework.data.annotation.Id;
@@ -9,47 +10,12 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
     @Id
     private ObjectId id;
-
     private String title;
-
     private String content;
-
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
